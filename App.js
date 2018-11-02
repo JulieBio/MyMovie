e7d347b95c25bf42e834960b70bfdca92df3039d
JulieBio <juliebio69@gmail.com>
2018-11-02 15:58:07 +0100

    first commit

---
+++ App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+import MyMovie from './MyMovie.jsx'
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <MyMovie/>
+      </div>
+    );
+  }
+}
+
+export default App;
